0a26a73fa7d0611756588019c47282dddb80f828
joseph <joseohjohnsonjr71@gmail.com>
2026-01-18 19:12:42 -0600

    reorg file structure

--- flashcardjam/app.js
+++ flashcardjam/app.js
@@ -10,6 +10,16 @@ let selectedTechnology = null;
 let selectedCategory = null;
 let isRandomized = false;
 
+// Fisher-Yates shuffle algorithm
+function shuffleArray(array) {
+    const shuffled = [...array];
+    for (let i = shuffled.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    }
+    return shuffled;
+}
+
 // Get DOM elements
 const technologySelection = document.getElementById('technologySelection');
 const categorySelection = document.getElementById('categorySelection');
